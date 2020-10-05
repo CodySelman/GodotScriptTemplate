642602daee04a0ab3f30bc3f77c3a427e3b1e93d
Cody Selman <cody.selman@sharpspring.com>
2020-10-05 01:20:53 -0700

    added script template

---
+++ better_template.gd
@@ -0,0 +1,19 @@
+# tool (if this is a tool)
+# class_name ClassName in PascalCase (also use PascalCase when loading classes into a variable)
+extends %BASE%
+# docstring
+
+# signals (snake_case, past tense)
+# enums (PascalCase for enum names, CONSTANT_CASE for enum members)
+# constants (CONSTANT_CASE)
+# exported variables (snake_case)
+# public variables (snake_case)
+# private variables (snake_case)
+# onready variables (snake_case)
+
+# func _init():
+# func _ready():
+# other built-in virtual methods ( such as _process(delta) )
+
+# public methods (snake_case, virtual methods prepended with underscore)
+# private methods (snake_case, virtual methods prepended with underscore)
